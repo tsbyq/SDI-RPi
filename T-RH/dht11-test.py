b252c286f6ccd73fe1d98d6f9fd64bfa813a0ac1
Han Li <hanlibpd@gmail.com>
2019-09-09 22:09:12 -0700

    Update dht11-test.py
    Write to file with 10 second interval and save a batch every 720 lines.

--- T-RH/dht11-test.py
+++ T-RH/dht11-test.py
@@ -9,6 +9,10 @@ import time
 import dht11
 import RPi.GPIO as GPIO
 
+import pandas as pd
+from datetime import datetime
+
+
 #define GPIO 14 as DHT11 data pin
 Temp_sensor=14
 def main():
@@ -19,14 +23,36 @@ def main():
 #  lcd_init()
   instance = dht11.DHT11(pin = Temp_sensor)
 
+  v_time = []
+  v_T = []
+  v_RH = []
+  count = 0
+  batch = 0
   while True:
-        #get DHT11 sensor value
-        result = instance.read()
-        print"Temperature = ",result.temperature,"C"," Humidity = ",result.humidity,"%"
-        time.sleep(1)
+      #get DHT11 sensor value
+      result = instance.read()
+      if result.temperature != 0 and result.humidity != 0:
+          count += 1
+          # Get current time
+          now = datetime.now()
+          time.sleep(10)
+          dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+          v_time.append(dt_string)
+          v_T.append(result.temperature)
+          v_RH.append(result.humidity)
+          print"Timestamp =", dt_string,", Temperature = ",result.temperature,"C"," Humidity = ",result.humidity,"%"
+      if count == 720:
+          print 'Save one batch...'
+          batch += 1
+          df = pd.DataFrame({
+                'Time': v_time,
+                'Temperature': v_T,
+                'Humidity': v_RH
+            })
+          df.to_csv('Measurement_batch_' + str(batch) + '.csv', index=False)
+          count = 0
 
 if __name__ == '__main__':
-
   try:
     main()
   except KeyboardInterrupt:
