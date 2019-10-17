eedd210e599ca22b9ef811675da85d114e704dc2
Han Li <hanlibpd@gmail.com>
2019-10-17 16:35:37 -0700

    Update PIR.py

--- Motion/PIR/PIR.py
+++ Motion/PIR/PIR.py
@@ -1,20 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN) #PIR
-GPIO.setup(24, GPIO.OUT) #BUzzer
+# GPIO.setup(24, GPIO.OUT) #BUzzer
 
 try:
     time.sleep(2) # to stabilize sensor
     while True:
         if GPIO.input(23):
-            GPIO.output(24, True)
-            time.sleep(0.5) #Buzzer turns on for 0.5 sec
-            GPIO.output(24, False)
-            print("Motion Detected...")
-            time.sleep(5) #to avoid multiple detection
+            # GPIO.output(24, True)
+            # time.sleep(0.5) #Buzzer turns on for 0.5 sec
+            # GPIO.output(24, False)
+            time = now.strftime("%Y-%M-%d_%H-%M-%S")
+            print(str(time) + "; Motion Detected...")
+            # Detect again
+            time.sleep(3) #to avoid multiple detection
         time.sleep(0.1) #loop delay, should be less than detection delay
 
 except:
